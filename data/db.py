89f1c04756bf7a9faf12bcd986fc6ed830443e60
Conner Hufnagel <conner.huf@gmail.com>
2025-02-07 10:05:01 -0800

    fix db connection

--- data/db.py
+++ data/db.py
@@ -1,6 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import config
 
-client = AsyncIOMotorClient(config.MONGODB_URI)
+client = AsyncIOMotorClient(
+    config.MONGODB_URI,
+    ssl=True,
+    tlsAllowInvalidCertificates=True)
 resume_db = client.resumeDB
 red_ribbon_db = client.redRibbon
